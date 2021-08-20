5d69052c6ba0d34fbfd192b51e94befd422aa0aa
Liubov <kravtsova5.0@yandex.ru>
2021-08-20 14:57:03 +0500

    some changes

---
+++ srcHW1.js
@@ -0,0 +1,20 @@
+"use strict"
+const products = [
+    { title: 'Shirt', price: 150 },
+    { title: 'Socks', price: 50 },
+    { title: 'Jacket', price: 350 },
+    { title: 'Shoes', price: 250 },
+];
+
+const renderProductItem = (title, price) => {
+    return `<div class="product-item"><h3>${title}</h3><p>${price}</p><button class="addButton">!!!</button></div>`;
+};
+
+const renderProductsList = (list) => {
+    let productListArr = list.map(item => renderProductItem(item.title, item.price));
+    let productList = productListArr.join("");
+    document.querySelector('.product-list').innerHTML = productList;
+
+}
+
+renderProductsList(products);
