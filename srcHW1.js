5fd8b2e93be0bb89a26eb0db4327d6d753adc586
Liubov <kravtsova5.0@yandex.ru>
2021-08-20 15:31:33 +0500

    some chadges

--- srcHW1.js
+++ srcHW1.js
@@ -7,14 +7,17 @@ const products = [
 ];
 
 const renderProductItem = (title, price) => {
-    return `<div class="product-item"><h3>${title}</h3><p>${price}</p><button class="addButton">!!!</button></div>`;
+    return `<div class="product-item">
+    <h3>${title}</h3>
+    <p>${price}</p>
+    <button class="mybutton" data-id="${title}"> Добавить</button>
+    </div>`;
 };
 
 const renderProductsList = (list) => {
     let productListArr = list.map(item => renderProductItem(item.title, item.price));
     let productList = productListArr.join("");
     document.querySelector('.product-list').innerHTML = productList;
-
 }
 
 renderProductsList(products);
