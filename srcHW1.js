e80e9d4c4bf67fd5e1a0c0bb02eb1303c6de110b
Liubov <kravtsova5.0@yandex.ru>
2021-08-24 12:08:11 +0500

    add product

--- srcHW1.js
+++ srcHW1.js
@@ -12,23 +12,94 @@ const products = [
 
 const cart = {
     products,
-    total: null,
-    list: null,
     idOfProduct: null,
     productListArr: [],
     productList: {},
+    el: null,
 
+    init() {
+        this.renderProductsList();
+        this.eventHandlers();
+
+    },
 
     renderProductsItem(product) {
         return `<div class="products-item">
         <h3>${product.title}</h3>
-        <p>${product.price}</p></div>`;
+        <p>${product.price}</p>
+        <button class="add-product" data-id="${product.title}"> Добавить</button>
+        </div>`;
     },
 
     renderProductsList() {
         document.querySelector('.products-list').innerHTML = products.map(item => this.renderProductsItem(item)).join('');
+        this.el = document.querySelector('.add-product');
+    },
+    eventHandlers() {
+        //document.querySelector('.add-product').addEventListener('click', event => this.addToBasket(event));
+        console.log('ifeventHandlers');
+        this.el.addEventListener('click', event => this.addToBasket(event));
+    },
+
+    addToBasket(event) {//увеличение количества
+        console.log('передаем элемент', event.target.dataset);
+        order.addToBasket(this.products.find((products) => products.title === event.target.dataset.id))
     },
 
 };
 
-cart.renderProductsList(products);
+const totalPriceStr = {
+    textReturn(product) {
+        return `<div><i><p> ${product.qt} ${product.id}</p></i></div>`;
+    }
+
+};
+
+const order = {
+    basket: [],
+
+    addToBasket(product) {
+        const inBascket = this.basket.find((element) => product.id === element.id);
+        if (!inBascket) {
+            this.basket.push({ ...product, qt: 1 });
+        } else {
+            inBascket.qt += 1;
+        }
+
+        this.init();
+
+    },
+
+    init() {  //Позиционирование на HTML-странице
+        this.el = document.querySelector('.mybasket');
+        console.log('инициализируем bascket ', this.basket);
+        this.basketRender();
+        this.eventHandlers();
+    },
+
+    basketRender() {
+        console.log('сейчас корзина такая:', this.basket);
+        this.clrBasketRender();//очистка корзины
+        this.el.insertAdjacentHTML('beforeend', `<div><i><h2> В корзине: </h2></i></div>`);
+        this.basket.forEach((element) => {
+            this.el.insertAdjacentHTML('beforeend', `${totalPriceStr.textReturn(element)}`);
+        }
+        );
+        this.el.insertAdjacentHTML('beforeend', '<button class="mybuttonCLr" data-id="${product.id}"> Удалить</button>');
+
+    },
+    clrBasketRender() {
+        this.el.innerHTML = '';
+    },
+    eventHandlers() {
+        this.el.addEventListener('click', event => this.clrBasket(event));
+    },
+    clrBasket() {
+        this.basket = [];
+        this.clrBasketRender();
+    },
+
+
+}
+
+cart.init();
