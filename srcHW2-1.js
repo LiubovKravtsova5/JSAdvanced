493ea3cc31055d969588dfe96b269f4ce1adc641
Liubov <kravtsova5.0@yandex.ru>
2021-08-27 19:48:38 +0500

    HW2-1&HW2-2

--- srcHW2-1.js
+++ srcHW2-1.js
@@ -30,6 +30,7 @@ class ProductsItem {
         this.img = img;
         this.id;
         this.text;
+        this.totalSum = null;
     }
     init() {
         let target = document.querySelector('.solution2-1');
@@ -50,7 +51,6 @@ class ProductsItem {
     eventHandlers() {
         this.id = document.getElementById(this.title);
         this.id.addEventListener('click', event => newBasket.addToBasket(event.target.dataset.id));
-
     }
 
 }
@@ -61,45 +61,71 @@ class Order {
         this.basket = [];
         this.text = '';
         this.target;
+        this.textB;
+        this.idB;
+        this.total = null;
     }
-
+    clrBusket() {
+        this.clrRenderOrder();
+        this.basket = [];
+        this.total = null;
+    };
     clrRenderOrder() {
-        this.text = '';
+        this.target = document.querySelector('.mybasket');
+        this.target.innerHTML = '';
     }
     addToBasket(id) {//увеличение количеств
         const inBascket = newBasket.basket.find((element) => id === element.title);
         if (!inBascket) {
             const pushProduct = pl.products.find((element) => id === element.title);
-            newBasket.basket.push({ ...pushProduct, qt: 1 });
+            newBasket.basket.push({ ...pushProduct, qt: 1, subSum: pushProduct.price });
         } else {
             const plusQt = newBasket.basket.findIndex((element) => id === element.title);
             newBasket.basket[plusQt]["qt"] += 1;
-
+            newBasket.basket[plusQt]["subSum"] = newBasket.basket[plusQt]["qt"] * newBasket.basket[plusQt]["price"];
         }
-
         this.init();
     }
-
+    clrButton() {
+        this.textB = document.createElement('p');
+        this.textB.innerHTML = '<button class="add - product" id="clrButton">Очистить корзину</button>';
+        this.target.insertAdjacentElement('beforeend', this.textB);
+    }
+    eventHandlers() {
+        this.idB = document.getElementById("clrButton");
+        this.idB.addEventListener('click', event => this.clrBusket());
+    }
     init() {
-        this.target = document.querySelector('.mybasket');
-        this.target.innerHTML = '';
-        console.log(this.basket);
+        this.clrRenderOrder();
         this.basket.forEach((item) => {
-            console.log(item);
-
-
             this.text = document.createElement('p');
             this.renderOrderItem(item);
             this.target.insertAdjacentElement('beforeend', this.text);
         });
+        this.totalSum();
+        this.clrButton();
+        this.eventHandlers();
     }
+    totalSum() {
+        this.total = null;
+        this.basket.forEach((item) => {
+            this.total += item.subSum;
+            console.log(item, "+", item.price, "=", this.total);
+        });
+        this.totalSumPlace = document.createElement('p');
+        this.totalSumPlace.innerHTML = `<div class="products-item">
+            <p><i><h3>Всего в корзине товаров на ${this.total}<h3></i></p>
+            </div>`;
+        this.target.insertAdjacentElement('beforeend', this.totalSumPlace);
 
+    }
     renderOrderItem(item) {
-
         this.text.innerHTML = `<div class="products-item">
         <p><i>${item.qt}${item.title} по цене: ${item.price} Подитог: ${item.subSum}</i></p>
         </div>`;
     }
+
+
 
 
 }
